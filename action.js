d6a9cac6eaa4b9457abed1e2a6fcd2959c713dbc
keakuyeng <89597221+keakuyeng@users.noreply.github.com>
2024-09-24 02:03:04 +0700

    Add files via upload

---
+++ action.js
@@ -0,0 +1,18 @@
+function img1() {
+  document.getElementById("img7").setAttribute("src", "./images/pic1.jpg");
+}
+function img2() {
+  document.getElementById("img7").setAttribute("src", "./images/pic2.jpg");
+}
+function img3() {
+  document.getElementById("img7").setAttribute("src", "./images/pic3.jpg");
+}
+function img4() {
+  document.getElementById("img7").setAttribute("src", "./images/pic4.jpg");
+}
+function img5() {
+  document.getElementById("img7").setAttribute("src", "./images/pic5.jpg");
+}
+function img6() {
+  document.getElementById("img7").setAttribute("src", "./images/pic6.jpg");
+}
